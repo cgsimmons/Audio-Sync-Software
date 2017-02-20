c1d73138e0b2648b894750990681f54554f422b0
cgsimmons <chris.simmons.alt@gmail.com>
2017-02-19 21:12:13 -0800

    Add simple play

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import Exponent from 'exponent';
+import Exponent, {
+  Asset,
+  Audio,
+} from 'exponent';
 import React from 'react';
 import {
   StyleSheet,
@@ -8,9 +11,31 @@ import {
 } from 'react-native';
 
 class App extends React.Component {
-  playSong = (event)=> {
-    console.log("Play Song");
+  constructor(props) {
+    super(props);
   }
+
+  playSong = async (event)=> {
+    console.log("Play Song");
+    const sound = new Audio.Sound({
+      source: require('./assets/sounds/test.mp3'),
+    });
+    await sound.loadAsync();
+    console.log(sound.isLoaded());
+    sound.play();
+  }
+
+  async getLoadedSound() {
+    if (this.sound == null) {
+      await Asset.fromModule('./assets/sounds/test.mp3').downloadAsync();
+    }
+    return await this.sound.loadAsync();
+  }
+
+  componentDidMount() {
+    Audio.setIsEnabled(true);
+  }
+
   render() {
     return (
       <View style={styles.container}>
